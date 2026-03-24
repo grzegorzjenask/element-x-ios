86cea87c0f11886d085999b150240350ec76e9fa
Doug <douglase@element.io>
2026-03-24 17:06:44 +0000

    Fix the release script.

--- Tools/Sources/Commands/CI/ReleaseToGithub.swift
+++ Tools/Sources/Commands/CI/ReleaseToGithub.swift
@@ -50,7 +50,7 @@ struct ReleaseToGitHub: AsyncParsableCommand {
         }
 
         // Bump the patch version using sed (preserves file formatting)
-        try await CI.run(.name("sed"), ["-i", "''", "'s/MARKETING_VERSION: \(currentVersion)/MARKETING_VERSION: \(newVersion)/g'", targetFilePath])
+        try await CI.run(.name("sed"), ["-i", "", "s/MARKETING_VERSION: \(currentVersion)/MARKETING_VERSION: \(newVersion)/g", targetFilePath])
         logger.info("Version updated from \(currentVersion) to \(newVersion)")
 
         try await CI.run(.name("xcodegen"))
